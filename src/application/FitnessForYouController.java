d70675632a601e3f73a15aa073dfff8008381c5a
AlinaMansuri <alina.mansuri@ucalgary.ca>
2022-11-19 04:41:32 +0000

    added controller to main scene

---
+++ src/application/FitnessForYouController.java
@@ -0,0 +1,26 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class FitnessForYouController {
+
+    @FXML
+    private ChoiceBox<?> weekExerciseChoiceBox;
+
+    
+
+    @FXML
+    void bmiCalculate(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void workoutDisplay(ActionEvent event) {
+    	
+    }
+
+}
+
+
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,8 @@
 module FitnessForYouApplication {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
+	requires java.desktop;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
